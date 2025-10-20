cd4a64ff8dc55fd3667630682c80c2b99c000265
johnwo90-JL <john-age@jobloop.no>
2025-10-20 11:35:45 +0200

    Chore: clean up, add comments

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-
+///////////////////////////
 // ## Asynkron Javascript
 
 
+////////////////////////
 // Bruk av setTimeout:
 console.log("[setTimeout] Start!");
 
@@ -11,7 +12,7 @@ setTimeout(() => {
 
 console.log("[setTimeout] Slutt!");
 
-
+////////////////////////////////////////////////////
 // Alternativet til asynkron JS - "Callback Hell":
 function getData(callback) {
     setTimeout(() => {
@@ -34,7 +35,7 @@ getData(data => {
 });
 
 
-
+//////////////////////////////////////////////////
 // En enkel asynkron funksjon, bruker "Promise":
 const foo = true;
 function fetchData(data) {
@@ -47,7 +48,7 @@ function fetchData(data) {
     });
 }
 
-
+//////////////////////////////////////////////////////////////
 // Funksjoner som ikke trenger parameter kan defineres slik:
 const myAsyncFunction = new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -62,8 +63,9 @@ const myAsyncFunction = new Promise((resolve, reject) => {
 // ...eller
 // const result = await myAsyncFunction;
 
-
-// "Chaining" - Oppnås ved å kjøre flere asynkrone funksjoner etter hverandre,
+///////////////////////////////////////////////////////////////////////////////////
+// "Chaining" - Gjør det mulig å kjøre flere asynkrone funksjoner etter hverandre,
+// så lenge de foregående funksjonene kjører uten feil.
 console.log("[CHAINING] Start!");
  const bar = fetchData("[CHAINING] Some data")
     .then(res => processAsyncData(res)) // <-- Bruk resultatet fra den første funksjonen...
@@ -79,22 +81,20 @@ console.log("[CHAINING] Start!");
 
 
 
-// (async () => {
-//     await getData();
-
-//     fetch("https://jsonplaceholder.typicode.com/posts/1")
-//         .then(response => response.json())
-//         .then(data => console.log(data))
-//         .catch(error => console.error("Feil:", error))
-//         .finally(() => console.log("Forespørsel fullført 2"));
-// })();
+////////////////////////////////////////////////
+// Enkelt workaround for å kjøre asynkron kode
+// med async/await fra top-level scope:
+(async () => {
+    // ...asynkron kode her!
+})();
 
 
+///////////////////////////////////////
 // Async/Await, og Try/Catch/Finished
 
-// Som vi vet, er funksjoner som returnerer "Promise" i praksis
-// det samme som en funksjon definert som `async function ...` eller `async () => ...`
-function processAsyncData(data) { 
+// Som vi vet: Funksjoner som returnerer "Promise", er i praksis
+// det samme som funksjoner definert med `async function ...` eller `async () => ...`
+function processAsyncData(data) { // Returnerer Promise<any>
     return new Promise((res, rej) => { // async
         console.log("[ASYNC/AWAIT] Process data running!");
         setTimeout(()=>{
@@ -108,7 +108,8 @@ function processAsyncData(data) {
     });
 }
 
-async function getAsyncData() { // returnerer Promise<object>
+async function getAsyncData() { // Returnerer Promise<any>
+    // Hent data fra API ved bruk av `fetch`:
     const result = await fetch("https://jsonplaceholder.typicode.com/posts/1");
     
     // Sjekk om HTTP statuskode er mellom 200 og 299:
@@ -118,8 +119,11 @@ async function getAsyncData() { // returnerer Promise<object>
 
     const json = await result.json();
     console.log(json);
-    
+
+    // Throw error, hvis man ønsker å teste det...
     // throw new Error("Foobar"); // tilsvarende: `reject("Foobar"); return;`
+
+    // Returner `json`
     return json; // tilsvarende: `resolve(json); return;`
 };
 
